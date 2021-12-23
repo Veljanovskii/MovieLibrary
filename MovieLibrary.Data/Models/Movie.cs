33a7ef703f2fb696555fb69b6777ef8cc4c111d2
Milos Veljanovski <milosveljanovski@elfak.rs>
2021-12-23 17:55:46 +0100

    feat: slight model change & added migrations

--- MovieLibrary.Data/Models/Movie.cs
+++ MovieLibrary.Data/Models/Movie.cs
@@ -10,6 +10,7 @@ namespace MovieLibrary.Data.Models
         public int MovieId { get; set; }
         public string Caption { get; set; }
         public int ReleaseYear { get; set; }
+        public int MovieLength { get; set; }
         public DateTime InsertDate { get; set; }
         public DateTime? DeleteDate { get; set; }
     }
